7e9fb17f648a2b2f7c4527c4b05a47380b568bc0
waleedahmad2031-create <waleedahmad2031@gmail.com>
2026-07-31 17:08:39 +0300

    Upgrade Firebase SDK version and update appId

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCt9fXd077nuTZ696hgQaL_gn1vzRY8LHQ",
@@ -13,13 +14,10 @@ const firebaseConfig = {
   projectId: "rehlattejarah",
   storageBucket: "rehlattejarah.firebasestorage.app",
   messagingSenderId: "56157415045",
-  appId: "1:56157415045:web:d1c496a5cdc4d5f81e2bec"
+  appId: "1:56157415045:web:34528a4cb34501261e2bec"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-
-export const db = getFirestore(app);
-
 export const auth = getAuth(app);
+export const db = getFirestore(app);
