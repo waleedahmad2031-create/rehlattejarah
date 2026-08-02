4f48de853de6f1b2c534a2a251afcc43a094dc08
waleedahmad2031-create <waleedahmad2031@gmail.com>
2026-08-02 12:03:10 +0300

    Update admin.orders.js

--- admin.orders.js
+++ admin.orders.js
@@ -1,61 +1,180 @@
 // admin-orders.js
 
+
 import { db } from "./firebase.js";
 
+
 import {
-  collection,
-  getDocs,
-  orderBy,
-  query
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+collection,
+
+getDocs,
+
+updateDoc,
+
+doc,
+
+orderBy,
+
+query
+
+}
+
+from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 
-const ordersList = document.getElementById("ordersList");
+
+const ordersBox = document.getElementById("orders");
+
 
 
 async function loadOrders(){
 
-  const q = query(
-    collection(db,"orders"),
-    orderBy("createdAt","desc")
-  );
+
+if(!ordersBox) return;
 
 
-  const snapshot = await getDocs(q);
+
+ordersBox.innerHTML="";
 
 
-  ordersList.innerHTML = "";
+
+const q = query(
+
+collection(db,"orders"),
+
+orderBy("createdAt","desc")
+
+);
 
 
-  snapshot.forEach((doc)=>{
+
+const snap = await getDocs(q);
 
 
-    let order = doc.data();
+
+snap.forEach(item=>{
 
 
-    ordersList.innerHTML += `
-
-    <tr>
-
-    <td>${order.name}</td>
-
-    <td>${order.phone}</td>
-
-    <td>${order.products}</td>
-
-    <td>${order.total} ريال</td>
-
-    <td>${order.status}</td>
-
-    </tr>
-
-    `;
+const order = item.data();
 
 
-  });
+
+ordersBox.innerHTML += `
+
+
+<div class="product">
+
+
+<h3>🛒 طلب جديد</h3>
+
+
+<p>
+الاسم: ${order.name}
+</p>
+
+
+<p>
+الجوال: ${order.phone}
+</p>
+
+
+<p>
+العنوان: ${order.address}
+</p>
+
+
+<p>
+الطلبات: ${order.products}
+</p>
+
+
+<p>
+المجموع: ${order.total} ريال
+</p>
+
+
+<p>
+الحالة: ${order.status}
+</p>
+
+
+
+<select onchange="changeStatus('${item.id}',this.value)">
+
+
+<option value="جديد">
+جديد
+</option>
+
+
+<option value="تم التواصل">
+تم التواصل
+</option>
+
+
+<option value="تم التوصيل">
+تم التوصيل
+</option>
+
+
+</select>
+
+
+</div>
+
+
+`;
+
+
+
+});
 
 
 }
 
 
+
 loadOrders();
+
+
+window.changeStatus = async function(id,status){
+
+
+try{
+
+
+await updateDoc(
+
+doc(db,"orders",id),
+
+{
+
+status:status
+
+}
+
+);
+
+
+
+alert("تم تحديث حالة الطلب");
+
+
+loadOrders();
+
+
+
+}
+
+catch(error){
+
+
+alert(error.message);
+
+
+}
+
+
+
+};
